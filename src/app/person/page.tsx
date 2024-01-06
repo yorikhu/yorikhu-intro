c80e076cd39017348e6b52aaac19fd455b5486bd
yorikhu <shallowink1999@163.com>
2024-01-06 11:24:17 +0800

    feat: enrich individual plate

--- src/app/person/page.tsx
+++ src/app/person/page.tsx
@@ -1,5 +1,36 @@
 "use client";
 
+import styles from "./page.module.scss";
+import Image from "next/image";
+
 export default function Person() {
-  return <>个人信息</>;
+  return (
+    <div className={styles["person-page"]}>
+      <div className={styles["intro-part"]}>
+        <div className={styles["avatar-area"]}>
+          <Image
+            priority={true}
+            width={400}
+            height={400}
+            src="/yorikhu/ai-avatar.png"
+            alt=""
+          />
+        </div>
+        <div className={styles["brief-desc"]}>
+          <p>花名：卷卷</p>
+          <p>姓名：胡永琪</p>
+          <p>英文名：Yorik Hu</p>
+          <p>坐标：杭州</p>
+          <p>爱好：敲代码、书籍、绘画</p>
+          <p>技术：JavaScript、HTML + CSS、React、Vue、Node</p>
+        </div>
+      </div>
+      <div className={styles["main-desc"]}>
+        <h2>写在前面</h2>
+        <div className={styles["desc-content"]}>
+          <p>我是一枚 99 年的在职前端开发</p>
+        </div>
+      </div>
+    </div>
+  );
 }
